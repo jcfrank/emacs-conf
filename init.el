3f956d5ed5346efae8ac77dfa2eb446844d9193b
Chen Chao Shih (Frank) <jcfrank7@gmail.com>
2014-01-03 16:56:56 +0800

    add to let backup files appear in emacs.d/backup/

--- init.el
+++ init.el
@@ -10,3 +10,11 @@
 (setq font-lock-mode 1)
 ;; == search highlight ==
 (setq search-highlight 1)
+;; == let backup files saved in emacs.d/backup ==
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
