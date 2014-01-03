36579edadb977a2ba88cd86367ff4ec0d2ba51ad
Chen Chao Shih (Frank) <jcfrank7@gmail.com>
2014-01-03 15:41:42 +0800

    add the init.el file. (same as ~/.emacs)

---
+++ init.el
@@ -0,0 +1,12 @@
+;; == set standard indent ==
+(setq standard-indent 4)
+;; == set default indent to use spaces ==
+(setq indent-tabs-mode nil)
+;; == show line number ==
+(setq line-number-mode 1)
+;; == show column number ==
+;; (column-number-mode 1)
+;; == syntax highlight mode ==
+(setq font-lock-mode 1)
+;; == search highlight ==
+(setq search-highlight 1)
