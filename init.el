e6e92b163f99810bf310512c6bf40da03d15f94e
Chen Chao Shih (Frank) <jcfrank7@gmail.com>
2014-01-03 18:18:20 +0800

    add ELPA. use M-x list-packages to see packages. i to mark install, x to execute.

--- init.el
+++ init.el
@@ -17,4 +17,10 @@
   delete-old-versions 1  ; Automatically delete excess backups
   kept-new-versions 20   ; how many of the newest versions to keep
   kept-old-versions 5    ; and how many of the old
-  )
+)
+;; == add ELPA package management ==
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+)
