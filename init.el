d435d15b4c83face1f5dc86004a3d38f984542bf
jcfrank7 <jcfrank7@gmail.com>
2015-01-10 15:19:43 +0800

    Add edts and remove some packages.

--- init.el
+++ init.el
@@ -18,11 +18,13 @@
   kept-new-versions 20   ; how many of the newest versions to keep
   kept-old-versions 5    ; and how many of the old
 )
-;; == add ELPA package management ==
+;; == add ELPA, MELPA package management ==
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
   (add-to-list 'package-archives '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+  (add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/"))
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (package-initialize)
 )
 ;; == start company-mode (auto-complete) ==
@@ -32,10 +34,8 @@
 (when (require 'auto-complete nil :noerror)
   (global-auto-complete-mode t))
 ;; == define default packages ==
-(defvar jcfrank/packages '(starter-kit-ruby
-			   ac-c-headers
+(defvar jcfrank/packages '(ac-c-headers
                            company
-                           company-go
                            erlang
                            edts
                            json-mode
@@ -44,7 +44,7 @@
                            auto-complete
                            go-mode
                            bash-completion
-                           enh-ruby-mode)
+                           exec-path-from-shell)
   "Default packages")
 ;; == check and install default packages ==
 (defun jcfrank/packages-installed-p ()
@@ -64,6 +64,18 @@
 (if (eq system-type 'darwin)
   (set-frame-font "Monaco 13" nil t)
 )
+
+;;add after-init-hook to init edts
+;;EDTS has a weird bug. If it is just installed, we need to go to
+;;"~/.emacs.d/elpa/edts-..../" and run "make".
+;;Otherwise there would be server not started error.
+(add-hook 'after-init-hook 'init-modes)
+(defun init-modes ()
+  (require 'edts-start))
+
+;;copy PATH from SHELL to exec-path when in osx
+(when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
