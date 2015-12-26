ad42502758365c2f88269a0e09b09366c53f52ba
jcfrank7 <jcfrank7@gmail.com>
2015-12-26 12:04:06 +0800

    Updates for format consistency and readability.

--- init.el
+++ init.el
@@ -1,52 +1,44 @@
-;; == set standard indent ==
-(setq standard-indent 2)
-;; == set default indent to use spaces ==
+;; set standard indent
+(setq standard-indent 4)
+;; set default indent to use spaces
 (setq indent-tabs-mode nil)
-;; == show line number ==
-(line-number-mode 1)
-;; == show column number ==
-(column-number-mode 1)
-;; == syntax highlight mode ==
-(global-font-lock-mode t)
-;; == search highlight ==
+;; show line number
+(setq line-number-mode t)
+;; show column number
+(setq column-number-mode t)
+;; syntax highlight mode
+(setq global-font-lock-mode t)
+;; search highlight
 (setq search-highlight t)
-;; == highlight parentheses ==
-(show-paren-mode 1)
-;; == no tab ==
+;; highlight parentheses
+(setq show-paren-mode t)
+;; no tab
 (setq-default indent-tabs-mode nil)
 
-;; == let backup files saved in emacs.d/backup ==
+;; let backup files saved in emacs.d/backup
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
   backup-by-copying 1    ; Don't delink hardlinks
   version-control 1      ; Use version numbers on backups
   delete-old-versions 1  ; Automatically delete excess backups
-  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-new-versions 10   ; how many of the newest versions to keep
   kept-old-versions 5    ; and how many of the old
 )
 
-;; == add ELPA, MELPA package management ==
+;; add MELPA, Marmalade into package management
 (when (>= emacs-major-version 24)
   (require 'package)
+  (add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/")) ; gnu
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  (add-to-list 'package-archives '("elpy" . "http://jorgenschaefer.github.io/packages/"))
-  (add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/"))
   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; for org mode
   (package-initialize)
 )
 
-;; == add ~/.emacs.d/ into load-path
+;; add ~/.emacs.d/ into load-path
 (add-to-list 'load-path 
   (expand-file-name "lisp" user-emacs-directory))
 
-;; == start company-mode (auto-complete) ==
-(when (require 'company nil :noerror)
-  (global-company-mode t))
-;; == start auto-complete-mode
-(when (require 'auto-complete nil :noerror)
-  (global-auto-complete-mode t))
-
-;; == define default packages ==
+;; define default packages
 (defvar default-packages '(org
                            find-file-in-project
                            auto-complete
@@ -69,7 +61,7 @@
                            xcscope)
   "Default packages")
 
-;; == check and install default packages ==
+;; check and install default packages
 (defun check-default-packages ()
   (setq res t)
   (dolist (pkg default-packages)
@@ -83,41 +75,48 @@
     (when (not (require pkg nil :noerror))
       (package-install pkg))))
 
-;;set font family for osx
-(if (eq system-type 'darwin)
-  (set-frame-font "Courier 12" nil t)
-)
+;; start company-mode (auto-complete)
+(require 'company)
+(global-company-mode t)
 
-;;copy PATH from SHELL to exec-path when in osx
-(when (memq window-system '(mac ns))
-    (exec-path-from-shell-initialize))
+;; start auto-complete-mode
+(require 'auto-complete)
+(global-auto-complete-mode t)
 
-;;add smex
-(when (require 'smex nil :noerror)
-    (smex-initialize)
-    (global-set-key (kbd "M-x") 'smex))
+;; add smex
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
 
-;;add expand-region
-(when (require 'expand-region nil :noerror)
-    (global-set-key (kbd "C-=") 'er/expand-region))
+;; add expand-region
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
 
-;;add powerline
-(when (require 'powerline nil :noerror)
-    (powerline-default-theme))
+;; add powerline
+(require 'powerline)
+(powerline-default-theme)
 
-;;add xcscope
-(when (require 'xcscope)
-    (cscope-setup))
+;; add xcscope
+(require 'xcscope)
+(cscope-setup)
 
 ;; erlang mode
-(require 'erlang-start)
+(add-hook 'erlang-mode-hook (lambda ()
+  (require 'erlang-start)
+  (setq standard-indent 2)
+))
 
 ;; find-file-in-project
-(when (require 'find-file-in-project nil :noerror)
-    (global-set-key (kbd "C-x p") 'find-file-in-project))
+(require 'find-file-in-project)
+(global-set-key (kbd "C-x p") 'find-file-in-project)
 
-;; set only for OS X Emacs.app
+;; ## set theme for OS X Emacs.app
 (defun set-osx-theme ()
+  ;; get env variables from shell
+  (exec-path-from-shell-copy-env "HOME")
+  (exec-path-from-shell-copy-env "USER")
+  (exec-path-from-shell-initialize)
+
   ;; set theme
   (custom-set-variables
    ;; custom-set-variables was added by Custom.
@@ -140,10 +139,10 @@
    '(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Droid_Sans_Mono_for_Powerline")))))
 
   ;; set pwd
-  (setq command-line-default-directory "/Users/jcfrank7")
+  (setq command-line-default-directory "~/")
 )
 
-;; set for Emacs in X
+;; set theme for Emacs in X
 (defun set-x-theme ()
   ;; set frame size
   (set-frame-size (selected-frame) 80 30)
@@ -165,8 +164,17 @@
    '(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 121 :width normal :foundry "monotype" :family "Monospace")))))
 )
 
+;; set theme for terminal
+(defun set-term-theme ()  
+  ;; set font faces for OS X
+  (case system-type 
+    ('darwin (set-frame-font "Courier 12" nil t))
+  )
+)
+
 ;; set env by runtime
 (case window-system
   ('ns (set-osx-theme))
   ('x (set-x-theme))
+  ('nil (set-term-theme))
 )
