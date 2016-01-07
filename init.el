e7a4bd05484251de08054d7bdce060ed1b28f972
Frank Chen <frankchen@exosite.com>
2016-01-07 16:59:19 +0800

    Disable ido-everywhere.

--- init.el
+++ init.el
@@ -158,7 +158,7 @@
 ;; flx-ido for fuzzy ido
 (require 'flx-ido)
 (ido-mode 1)
-(ido-everywhere 1)
+;(ido-everywhere 1)
 (flx-ido-mode 1)
 (setq ido-enable-flex-matching t)
 (setq ido-use-faces nil)
